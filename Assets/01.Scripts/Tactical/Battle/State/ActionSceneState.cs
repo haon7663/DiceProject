a342fc38fe0892e8096e42aba34d617061c2156d
haon <89632611+haon7663@users.noreply.github.com>
2024-08-27 00:35:14 +0900

    update : actionScene, sprite

---
+++ Assets/01.Scripts/Tactical/Battle/State/ActionSceneState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionSceneState : BattleState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        StartCoroutine(Action());
+    }
+
+    private IEnumerator Action()
+    {
+        owner.interactionPanelController.Hide();
+        yield return YieldInstructionCache.WaitForSeconds(1f);
+            
+        owner.mainCameraMovement.VibrationForTime(0.65f);
+        owner.mainCameraMovement.ProductionAtTime(new Vector3(-0.4f, 0.35f, -10), 3, 4.4f);
+        owner.highlightCameraMovement.VibrationForTime(0.5f);
+        owner.highlightCameraMovement.ProductionAtTime(new Vector3(-0.4f, 0.35f, -10), 1, 4f);
+        owner.mainCameraVolumeSettings.SetVolume();
+        
+        var from = Turn.isPlayer ? owner.player : owner.enemy;
+        var to = Turn.isPlayer ? owner.enemy : owner.player;
+        
+        if (from.TryGetComponent<Act>(out var fromAct) && to.TryGetComponent<Act>(out var toAct))
+        {
+            fromAct.AttackAction();
+            toAct.HitAction();
+        }
+        
+        yield return YieldInstructionCache.WaitForSeconds(1.2f);
+        
+        owner.mainCameraMovement.ProductionAtTime(new Vector3(0, 0, -10), 0, 5, true);
+        owner.highlightCameraMovement.ProductionAtTime(new Vector3(0, 0, -10), 0, 5, true);
+        owner.mainCameraVolumeSettings.ResetVolume();
+        
+        owner.interactionPanelController.Show();
+
+        yield return null;
+    }
+}
---
+++ Assets/01.Scripts/Tactical/UI/Controller/InteractionPanelController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPanelController : MonoBehaviour
+{
+    [SerializeField] private Panel panel;
+
+    private void Start()
+    {
+        panel.SetPosition(PanelStates.Show);
+    }
+
+    public void Show()
+    {
+        panel.SetPosition(PanelStates.Show, true, 0.5f);
+    }
+    
+    public void Hide()
+    {
+        panel.SetPosition(PanelStates.Hide, true, 1f);
+    }
+}
