f3ed3d90d5a82f5be88176f95408a591ef19758e
haon <89632611+haon7663@users.noreply.github.com>
2024-08-31 00:01:01 +0900

    fix : FourDiceAnimation

--- Assets/01.Scripts/Tactical/Battle/State/DiceRollingState.cs
+++ Assets/01.Scripts/Tactical/Battle/State/DiceRollingState.cs
@@ -10,35 +10,41 @@ public class DiceRollingState : BattleState
     public override void Enter()
     {
         base.Enter();
-        StartCoroutine(Rolling());
+        StartCoroutine(RollingDices());
     }
 
-    public IEnumerator Rolling()
+    public IEnumerator RollingDices()
     {
         var unit = owner.player;
-        var diceTypes = unit.cardSO.cardEffects.SelectMany(cardEffect => cardEffect.diceTypes).ToList();
-        yield return StartCoroutine(RollingDice(diceTypes));
-    }
-    
-    public IEnumerator RollingDice(List<DiceType> diceTypes)
-    {   
+        var cardEffects = unit.cardSO.cardEffects;
+
+        for (int i = 0; i < cardEffects.Count; i++)
+        {
+            
+        }
+
         for (var i = 0; i < diceTypes.Count; i++)
         {
             var diceType = diceTypes[i];
-            
-            if (owner.PlayerData.dices[diceType] <= 0) continue;
-            DataManager.Inst.PlayerData.dices[diceType]--;
-            
-            var position = new Vector3(2.5f * (i - (float)(diceTypes.Count - 1) / 2) - 10, 0) + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)) * 0.4f;
-            var rotation = Quaternion.Euler(0, Random.Range(-15f, 15f), 0);
-            
-            var dice = DiceFactory.Create(diceType);
-            dice.transform.position = position;
-            dice.transform.rotation = rotation;
-            
-            yield return YieldInstructionCache.WaitForSeconds(Random.Range(0.15f, 0.225f));
 
-            owner.diceResultPanelController.AddValue(owner.player, diceType.GetDiceValue());
+            var defaultPos = new Vector3(2.5f * (i - (float)(diceTypes.Count - 1) / 2) - 10, 0);
+            var randPos = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)) * 0.4f;
+            var pos = defaultPos + randPos;
+            RollingDice(diceType, pos);
         }
+    }
+    
+    private int RollingDice(DiceType diceType, Vector3 pos)
+    {
+        if (owner.PlayerData.dices[diceType] <= 0) return 0;
+        DataManager.Inst.PlayerData.dices[diceType]--;
+        
+        var value = diceType.GetDiceValue();
+            
+        var dice = DiceFactory.Create(diceType);
+        dice.transform.position = pos;
+        dice.transform.rotation = Quaternion.Euler(0, Random.Range(-15f, 15f), 0);
+
+        return diceType.GetDiceValue();
     }
 }
