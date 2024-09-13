33ba3177ac97689e12c6ee611d5fedfa1e1ea7de
haon <89632611+haon7663@users.noreply.github.com>
2024-09-13 22:32:56 +0900

    add : PrepareBattleState

---
+++ Assets/01.Scripts/Tactical/Battle/State/PrepareBattleState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+public class PrepareBattleState : BattleState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        StartCoroutine(PrepareBattle());
+    }
+
+    private IEnumerator PrepareBattle()
+    {
+        if (owner.player.TryGetComponent<Act>(out var playerAct))
+            playerAct.Init();
+        if (owner.enemy.TryGetComponent<Act>(out var enemyAct))
+            enemyAct.Init();
+
+        yield return YieldInstructionCache.WaitForSeconds(1f);
+        
+        owner.ChangeState<TurnChangeState>();
+    }
+}
