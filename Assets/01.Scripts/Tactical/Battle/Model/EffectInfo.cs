4104eee8fadf7cda04e505e6b8dbf519d7ad1a96
haon <89632611+haon7663@users.noreply.github.com>
2024-09-15 21:18:45 +0900

    feat : Item Effect

---
+++ Assets/01.Scripts/Tactical/Battle/Model/EffectInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectInfo
+{
+    [Header("#BehaviourType")]
+    public BehaviourType behaviourType;
+    
+    [DrawIf("behaviourType", BehaviourType.StatusEffect)]
+    public StatusEffectSO statusEffectSO;
+    
+    [Header("#Value")]
+    public int basicValue;
+    public List<DiceType> diceTypes;
+    
+    [Header("#Compare")]
+    public CompareInfo compareInfo;
+    
+    [Header("#Special")]
+    public bool onSelf;
+    public SpecialBehaviourType specialBehaviourType;
+}
