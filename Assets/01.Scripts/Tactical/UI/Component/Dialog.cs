4e493b1e7df1d09be7214f5e6705ea69db41f92f
haon <89632611+haon7663@users.noreply.github.com>
2024-09-16 01:01:32 +0900

    update : DialogController, UnderMark

--- Assets/01.Scripts/Tactical/UI/Component/Dialog.cs
+++ Assets/01.Scripts/Tactical/UI/Component/Dialog.cs
@@ -1,6 +1,11 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class Dialog : MonoBehaviour
 {
-        
+    [SerializeField] private TMP_Text description;
+    public void SetAlpha(float value)
+    {
+        description.color = new Color(description.color.r, description.color.g, description.color.b, value);
+    }
 }
--- Assets/01.Scripts/Tactical/UI/Controller/DialogController.cs
+++ Assets/01.Scripts/Tactical/UI/Controller/DialogController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,29 +12,62 @@ public class DialogController : MonoBehaviour
     
     [SerializeField] private ScrollRect scrollRect;
     [SerializeField] private RectTransform contentRect;
+    [SerializeField] private Image underMark;
+
+    private List<Dialog> _dialogs = new List<Dialog>();
+
+    private bool _isShown;
     
     [ContextMenu("GenerateDialog")]
     public void GenerateDialog()
     {
         var dialog = Instantiate(dialogPrefab, contentRect.transform);
+        _dialogs.Add(dialog);
         SetParentPosition(dialog);
     }
-    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            GenerateDialog();
+
+        foreach (var dialog in _dialogs)
+        {
+            var dialogPos = scrollRect.transform.InverseTransformPoint(dialog.transform.position);
+
+            var alpha = dialogPos.y switch
+            {
+                < 0 => Mathf.Clamp01(1 - Mathf.Abs(dialogPos.y) / 50f),
+                > 100 => Mathf.Clamp01(1 - (dialogPos.y - 100) / 50f),
+                _ => 1f
+            };
+
+            dialog.SetAlpha(alpha);
+        }
+
+        if (scrollRect.transform.InverseTransformPoint(_dialogs.Last().transform.position).y < -65f && !_isShown)
+        {
+            DOTween.Kill(underMark);
+            _isShown = true;
+            underMark.DOFade(1, 0.25f);
+        }
+        else if (scrollRect.transform.InverseTransformPoint(_dialogs.Last().transform.position).y >= -65f && _isShown)
+        {
+            DOTween.Kill(underMark);
+            _isShown = false;
+            underMark.DOFade(0, 0.25f);
+        }
+    }
+
     private void SetParentPosition(Dialog dialog)
     {
         DOTween.Kill(contentRect);
         Canvas.ForceUpdateCanvases();
-
+        
         Vector2 contentPos = scrollRect.transform.InverseTransformPoint(contentRect.position);
         Vector2 dialogPos = scrollRect.transform.InverseTransformPoint(dialog.transform.position);
         Vector2 offset = contentPos - dialogPos;
-
-        float contentMinY = -contentRect.sizeDelta.y;
-        float contentMaxY = 0f;
-
-        float targetPosY = Mathf.Clamp(offset.y + 540f, contentMinY, contentMaxY);
-
-        contentRect.DOAnchorPosY(targetPosY, 0.25f);
+        
+        contentRect.DOAnchorPosY(offset.y - 30, 0.25f);
     }
-
 }
