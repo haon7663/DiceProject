18eb7261a960991367cab8693e00db9eb26f05b0
haon <89632611+haon7663@users.noreply.github.com>
2024-09-25 15:14:55 +0900

    add : StatusExplainPanel

---
+++ Assets/01.Scripts/Tactical/UI/Component/StatusExplainPanel.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusExplainPanel : MonoBehaviour
+{
+    public Panel panel;
+
+    [SerializeField] private Image icon;
+    [SerializeField] private TMP_Text nameLabel;
+    [SerializeField] private TMP_Text descriptionLabel;
+
+    public void Initialize(StatusEffectSO statusEffect)
+    {
+        
+    }
+}
---
+++ Assets/01.Scripts/Tactical/UI/Controller/StatusExplainPanelController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusExplainPanelController : MonoBehaviour
+{
+    [SerializeField] private StatusExplainPanel explainPanelPrefab;
+    [SerializeField] private Transform parent;
+    
+    
+}
